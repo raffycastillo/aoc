84427c80d146fcb28779a5f9135348c25f59b6da
raffycastillo <78766072+raffycastillo@users.noreply.github.com>
2025-11-19 19:00:06 -0500

    2025 pre-init

---
+++ 2025/get.py
@@ -0,0 +1,8 @@
+# make my own ? that would be a fun little homework
+# https://pypi.org/project/advent-of-code-data/
+from aocd import get_data
+
+data = get_data(day=1, year=2025)
+input = open('7.in', 'a')
+input.write(data)
+input.close()
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ for posterity (also am noob)
 
 ## 2024
 first timer, also trying it out in python
+
+## 2025
+maybe we will do it in cpp (python as fallback :sob:)
