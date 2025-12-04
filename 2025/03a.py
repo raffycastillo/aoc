cc8c14899c86f2ec9ede9024748b06e984232990
raffycastillo <78766072+raffycastillo@users.noreply.github.com>
2025-12-03 19:05:26 -0500

    2025_03a

---
+++ 2025/03a.py
@@ -0,0 +1,47 @@
+# TEST FLAG setup
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--test', action='store_true', help='Use test input')
+args = parser.parse_args()
+
+problem_num = sys.argv[0].split('/')[-1][:2]
+input_file = f'{problem_num}-test.in' if args.test else f'{problem_num}.in'
+# TEST FLAG setup END
+
+# FILE READ setup
+input = open(input_file, 'r')
+data = input.read()
+input.close()
+lines = data.strip().split('\n')
+# FILE READ setup END
+
+res = 0
+
+for line in lines:
+    print('---')
+    print('for line:')
+    print(line)
+
+    left, right = 0, 1
+    line = [int(char) for char in line]
+    
+    maxJolt = line[left]*10 + line[right]
+    while right < len(line)-1:
+        if line[left] < line[right]:
+            left = right
+            right = left+1
+        else:
+            right = right+1
+        
+        curr = line[left]*10 + line[right]
+        maxJolt = max(maxJolt, curr)
+    
+    res += maxJolt
+    print('added: ', maxJolt)
+
+print('---')
+print('out:')
+print(res)
+
